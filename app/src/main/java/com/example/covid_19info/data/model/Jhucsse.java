e2370d34be8261943e54f150040edfe63fa9b393
Niroj Shrestha <shr.niroj08@gmail.com>
2020-03-29 18:10:59 +0545

    added model and dependencies

---
+++ app/src/main/java/com/example/covid_19info/data/model/Jhucsse.java
@@ -0,0 +1,59 @@
+package com.example.covid_19info.data.model;
+
+
+import com.google.gson.annotations.SerializedName;
+
+import org.parceler.Parcel;
+
+import java.util.Date;
+
+@Parcel(value = Parcel.Serialization.BEAN)
+public class Jhucsse {
+
+    @SerializedName("country")
+    private String country;
+    @SerializedName("province")
+    private String province;
+    @SerializedName("updatedAt")
+    private Date updatedAt;
+    @SerializedName("stats")
+    private Stats fee;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public Stats getFee() {
+        return fee;
+    }
+
+    private static class Stats {
+
+        @SerializedName("confirmed")
+        private int confirmed;
+        @SerializedName("deaths")
+        private int deaths;
+        @SerializedName("recovered")
+        private int recovered;
+
+        public int getConfirmed() {
+            return confirmed;
+        }
+
+        public int getDeaths() {
+            return deaths;
+        }
+
+        public int getRecovered() {
+            return recovered;
+        }
+    }
+}
