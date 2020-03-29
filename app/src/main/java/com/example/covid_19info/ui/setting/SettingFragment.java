e668afb74c977999517feb1a41e1106ae85ccb3d
Niroj Shrestha <shr.niroj08@gmail.com>
2020-03-29 19:01:11 +0545

    Changes

--- app/src/main/java/com/example/covid_19info/ui/setting/SettingFragment.java
+++ app/src/main/java/com/example/covid_19info/ui/setting/SettingFragment.java
@@ -1,4 +1,4 @@
-package com.example.covid_19info.ui.global;
+package com.example.covid_19info.ui.setting;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.covid_19info.R;
 
-public class GlobalFragment extends Fragment {
+public class SettingFragment extends Fragment {
 
-    private GlobalViewModel globalViewModel;
+    private SettingViewModel settingViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        globalViewModel =
-                ViewModelProviders.of(this).get(GlobalViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_global, container, false);
-        final TextView textView = root.findViewById(R.id.text_global);
-        globalViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        settingViewModel =
+                ViewModelProviders.of(this).get(SettingViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_settings, container, false);
+        final TextView textView = root.findViewById(R.id.text_setting);
+        settingViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
