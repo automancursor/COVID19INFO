01dc6d1a45a1cd27335733f77ca492e629c1aa8f
Sanam <sanam.dongol@gmail.com>
2020-03-29 23:46:43 +0545

    String formatting

--- app/src/main/java/com/example/covid_19info/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/covid_19info/ui/home/HomeFragment.java
@@ -14,42 +14,80 @@ import com.example.covid_19info.R;
 
 import java.util.Calendar;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    @BindView(R.id.text_updated)
+    TextView textUpdated;
+    @BindView(R.id.text_total_cases)
+    TextView totalCases;
+    @BindView(R.id.text_country_name)
+    TextView countryName;
+    @BindView(R.id.text_total_deaths)
+    TextView totalDeaths;
+    @BindView(R.id.text_total_today_cases)
+    TextView todayCases;
+    @BindView(R.id.text_total_today_deaths)
+    TextView todayDeaths;
+    @BindView(R.id.text_total_active_cases)
+    TextView totalActive;
+    @BindView(R.id.text_total_recovered)
+    TextView totalRecovered;
+    private Unbinder unbinder;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        unbinder = ButterKnife.bind(this, root);
 
-        final TextView textUpdated = root.findViewById(R.id.text_updated);
+       /* final TextView textUpdated = root.findViewById(R.id.text_updated);
         final TextView totalCases = root.findViewById(R.id.text_total_cases);
         final TextView countryName = root.findViewById(R.id.text_country_name);
         final TextView totalDeaths = root.findViewById(R.id.text_total_deaths);
         final TextView todayCases = root.findViewById(R.id.text_total_today_cases);
         final TextView todayDeaths = root.findViewById(R.id.text_total_today_deaths);
         final TextView totalActive = root.findViewById(R.id.text_total_active_cases);
-        final TextView totalRecovered = root.findViewById(R.id.text_total_recovered);
+        final TextView totalRecovered = root.findViewById(R.id.text_total_recovered);*/
 
         homeViewModel.getData().observe(getViewLifecycleOwner(), data -> {
 
             Calendar cal = Calendar.getInstance();
             cal.setTimeInMillis(data.getUpdated());
 
-            totalCases.setText("Total\n\n" + data.getCases());
-            totalActive.setText("Active\n\n" + data.getActive());
-            totalDeaths.setText("Deaths\n\n" + data.getDeaths());
+            totalCases.setText(formatText("Total", data.getCases()));
+            totalActive.setText(formatText("Active", data.getActive()));
+            totalDeaths.setText(formatText("Deaths", data.getDeaths()));
+
             countryName.setText(String.valueOf(data.getCountry()));
-            todayCases.setText("Today's Cases\n\n" + data.getTodayCases());
-            todayDeaths.setText("Today's Deaths\n\n" + data.getTodayDeaths());
-            totalRecovered.setText("Recovered\n\n" + data.getRecovered());
-            textUpdated.setText("Updated On : " + cal.get(Calendar.HOUR) + ":" + cal.get(Calendar.MINUTE) + " "
-                    + cal.get(Calendar.DATE) + "/" + (cal.get(Calendar.MONTH) + 1) + "/" + cal.get(Calendar.YEAR));
+
+            todayCases.setText(formatText("Today's Cases", data.getTodayCases()));
+            todayDeaths.setText(formatText("Today's Deaths", data.getTodayDeaths()));
+            totalRecovered.setText(formatText("Recovered", data.getRecovered()));
+
+
+            String sf = String.format("%s%s:%s %s/%s/%s", "Updated On :", cal.get(Calendar.HOUR), cal.get(Calendar.MINUTE), cal.get(Calendar.DATE),
+                    (cal.get(Calendar.MONTH) + 1), cal.get(Calendar.YEAR));
+            textUpdated.setText(sf);
+
 
         });
 
         return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    private String formatText(String title, int data) {
+        return String.format("%s\n\n %s", title, data);
     }
 }
