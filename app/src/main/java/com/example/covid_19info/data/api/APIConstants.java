a9c97fbe65d41086f112942fe646478a5840b4db
Niroj Shrestha <shr.niroj08@gmail.com>
2020-04-19 09:41:23 +0545

    Updates for the new URL

--- app/src/main/java/com/example/covid_19info/data/api/APIConstants.java
+++ app/src/main/java/com/example/covid_19info/data/api/APIConstants.java
@@ -5,10 +5,10 @@ package com.example.covid_19info.data.api;
  */
 class APIConstants {
 
-    static final String BASE_URL                    = "https://corona.lmao.ninja/";
+    static final String BASE_URL                    = "https://corona.lmao.ninja/v2/";
 
     static final String ALL                         = "all";
-    static final String JHUCSSE                     = "v2/jhucsse";
+    static final String JHUCSSE                     = "jhucsse";
     static final String COUNTRIES                   = "countries";
     static final String SOMETHING_WENT_WRONG        = "Something went wrong!";
     static final String PLEASE_TRY_AGAIN            = "Please try again.";
